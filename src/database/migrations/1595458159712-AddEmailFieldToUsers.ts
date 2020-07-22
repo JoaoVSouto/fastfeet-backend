db840b741649c7d3839876e29ecad963786769ee
JoaoVSouto <joaovitorss99@hotmail.com>
2020-07-22 19:52:23 -0300

    Adds migration for adding email field on users table

---
+++ src/database/migrations/1595458159712-AddEmailFieldToUsers.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class AddEmailFieldToUsers1595458159712 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'email',
+        type: 'varchar',
+        isNullable: false,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'email');
+  }
+}
