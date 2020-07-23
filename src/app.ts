94053da853c43c4ce71382fc63934959a8475292
JoaoVSouto <joaovitorss99@hotmail.com>
2020-07-23 09:02:55 -0300

    Modularizes express instance on a separate class

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import 'reflect-metadata';
+import 'express-async-errors';
+
+import express, { Express, Request, Response, NextFunction } from 'express';
+
+import './database';
+
+import AppError from '@errors/AppError';
+
+import routes from './routes';
+
+class App {
+  public readonly server: Express;
+
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+    this.exceptionHandler();
+  }
+
+  private middlewares() {
+    this.server.use(express.json());
+  }
+
+  private routes() {
+    this.server.use(routes);
+  }
+
+  private exceptionHandler() {
+    this.server.use(
+      (err: Error, req: Request, res: Response, _: NextFunction) => {
+        if (err instanceof AppError) {
+          return res.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+          });
+        }
+
+        return res.status(500).json({
+          status: 'error',
+          message: 'Internal server error',
+        });
+      },
+    );
+  }
+}
+
+export default new App().server;
--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,4 @@
-import 'reflect-metadata';
-import 'express-async-errors';
-import express, { Request, Response, NextFunction } from 'express';
-
-import './database';
-import AppError from '@errors/AppError';
-import routes from './routes';
-
-const app = express();
-
-app.use(express.json());
-
-app.use(routes);
-
-app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
-  if (err instanceof AppError) {
-    return res.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
-  }
-
-  return res.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
+import app from './app';
 
 // eslint-disable-next-line no-console
 app.listen(3333, () => console.log('Server listening on 3333...'));
