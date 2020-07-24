181f0e31d81f166d9f754efee4904018ecaef1db
JoaoVSouto <joaovitorss99@hotmail.com>
2020-07-24 20:36:34 -0300

    Filters incoming files by their types

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,11 +3,14 @@ import path from 'path';
 import multer, { Options } from 'multer';
 import { uuid as generateUuid } from 'uuidv4';
 
+import AppError from '@errors/AppError';
+
 export const uploadsDir = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default function multerConfig(uploadFolder: string): Options {
   return {
     dest: path.resolve(uploadsDir, uploadFolder),
+
     storage: multer.diskStorage({
       destination: path.resolve(uploadsDir, uploadFolder),
       filename(req, file, cb) {
@@ -19,5 +22,16 @@ export default function multerConfig(uploadFolder: string): Options {
         return cb(null, fileName);
       },
     }),
+
+    fileFilter(req, file, cb) {
+      const acceptedTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+
+      if (!acceptedTypes.includes(file.mimetype)) {
+        cb(new AppError('File type is not allowed'));
+        return;
+      }
+
+      cb(null, true);
+    },
   };
 }
