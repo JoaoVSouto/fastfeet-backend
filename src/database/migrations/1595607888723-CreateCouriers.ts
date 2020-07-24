a28374c397704ac002896f02f53ec5d916636ae1
JoaoVSouto <joaovitorss99@hotmail.com>
2020-07-24 13:45:20 -0300

    Adds migration for creating couriers table

---
+++ src/database/migrations/1595607888723-CreateCouriers.ts
@@ -0,0 +1,68 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateCouriers1595607888723 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'couriers',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'avatar_id',
+            type: 'integer',
+            isNullable: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'couriers',
+      new TableForeignKey({
+        name: 'CourierAvatar',
+        columnNames: ['avatar_id'],
+        referencedTableName: 'files',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('couriers', 'CourierAvatar');
+
+    await queryRunner.dropTable('couriers');
+  }
+}
