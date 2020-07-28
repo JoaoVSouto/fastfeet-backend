f0a950346452d86c0c738161c486dec7df9ee381
JoaoVSouto <joaovitorss99@hotmail.com>
2020-07-28 15:12:19 -0300

    Validates request on listing courier packages

--- src/routes/client/courier.routes.ts
+++ src/routes/client/courier.routes.ts
@@ -1,21 +1,27 @@
 import { Router } from 'express';
 
+import CourierPackageValidator from '@validators/CourierPackageValidator';
+
 import ListCourierPackagesService from '@services/client/ListCourierPackagesService';
 
 const routes = Router();
 
-routes.get('/:id/packages', async (req, res) => {
-  const { id } = req.params;
-  const { delivered } = req.query;
+routes.get(
+  '/:id/packages',
+  CourierPackageValidator.list(),
+  async (req, res) => {
+    const { id } = req.params;
+    const { delivered } = req.query;
 
-  const listCourierPackages = new ListCourierPackagesService();
+    const listCourierPackages = new ListCourierPackagesService();
 
-  const packages = await listCourierPackages.execute({
-    courier_id: Number(id),
-    delivered: String(delivered),
-  });
+    const packages = await listCourierPackages.execute({
+      courier_id: Number(id),
+      delivered: String(delivered),
+    });
 
-  return res.json(packages);
-});
+    return res.json(packages);
+  },
+);
 
 export default routes;
