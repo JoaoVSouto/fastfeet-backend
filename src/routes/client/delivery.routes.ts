202f2231d57973262e78b40bfbe9e245b0a5d214
JoaoVSouto <joaovitorss99@hotmail.com>
2020-08-10 10:17:25 -0300

    Validates request on showing package problems

--- src/routes/client/delivery.routes.ts
+++ src/routes/client/delivery.routes.ts
@@ -1,19 +1,25 @@
 import { Router } from 'express';
 
+import DeliveryProblemValidator from '@validators/DeliveryProblemValidator';
+
 import ShowDeliveryProblemsService from '@services/client/DeliveryProblem/ShowDeliveryProblemsService';
 
 const routes = Router();
 
-routes.get('/:id/problems', async (req, res) => {
-  const { id } = req.params;
+routes.get(
+  '/:id/problems',
+  DeliveryProblemValidator.show(),
+  async (req, res) => {
+    const { id } = req.params;
 
-  const showDeliveryProblems = new ShowDeliveryProblemsService();
+    const showDeliveryProblems = new ShowDeliveryProblemsService();
 
-  const problems = await showDeliveryProblems.execute({
-    package_id: Number(id),
-  });
+    const problems = await showDeliveryProblems.execute({
+      package_id: Number(id),
+    });
 
-  return res.json(problems);
-});
+    return res.json(problems);
+  },
+);
 
 export default routes;
